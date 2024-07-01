bbb9073dbeda705f034c344ff82abd871a362df9
姚子豪 <yaozihao@yaozihao.com>
2024-07-01 20:31:33 +1000

    feat: #103 add eslint prettierrc

--- src/app/activate/[userId]/page.tsx
+++ src/app/activate/[userId]/page.tsx
@@ -1,63 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import axios from 'axios';
-import '../styles/activate.css';
+// import React, { useState, useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import axios from 'axios';
+// import '../styles/activate.css'
 
-const Activate: React.FC = () => {
-  const [timerCounter, setTimerCounter] = useState(0);
-  const [buttonDisabled, setButtonDisabled] = useState(false);
-  const router = useRouter();
-  const { userId } = router.query; //dynamic params
+// const Activate: React.FC = () => {
+//   const [timerCounter, setTimerCounter] = useState(0);
+//   const [buttonDisabled, setButtonDisabled] = useState(false);
+//   const router = useRouter();
+//   const { userId } = router.query; //dynamic params
 
-  const sendActivateEmail = async () => {
-    setTimerCounter(60); // Reset the timer to 60 seconds
-    setButtonDisabled(true); // Disable the button
-    try {
-      await axios.post('/todo', { userId });
-      alert('prompt.email_has_sent');
-    } catch (e) {
-      alert(e.message || 'prompt.error_occurs');
-    }
-  };
+//   const sendActivateEmail = async () => {
+//     setTimerCounter(60); // Reset the timer to 60 seconds
+//     setButtonDisabled(true); // Disable the button
+//     try {
+//       await axios.post('/todo', { userId });
+//       alert('prompt.email_has_sent');
+//     } catch (e) {
+//       alert(e.message || 'prompt.error_occurs');
+//     }
+//   };
 
-  useEffect(() => {
-    if (timerCounter > 0) {
-      const timer = setTimeout(() => {
-        setTimerCounter(timerCounter - 1);
-      }, 1000);
+//   useEffect(() => {
+//     if (timerCounter > 0) {
+//       const timer = setTimeout(() => {
+//         setTimerCounter(timerCounter - 1);
+//       }, 1000);
 
-      return () => clearTimeout(timer); 
-    } else {
-      setButtonDisabled(false); 
-    }
-  }, [timerCounter]);
+//       return () => clearTimeout(timer);
+//     } else {
+//       setButtonDisabled(false);
+//     }
+//   }, [timerCounter]);
 
-  const buttonText = timerCounter > 0
-    ? `Wait ${timerCounter} seconds to resend`
-    : 'Resend activation email';
+//   const buttonText = timerCounter > 0
+//     ? `Wait ${timerCounter} seconds to resend`
+//     : 'Resend activation email';
 
-  return (
-    <section className="section">
-      <div className="container">
-        <div className="box">
-          <div className="header">
-            <p className="is-size-6 is-pulled-left">{'caption.account_not_active'}</p>
-          </div>
-          <div className="has-text-centered content">
-            <p>{'prompt.account_not_active'}</p>
-            <p>.</p>
-            <button
-              className="button is-link"
-              onClick={sendActivateEmail}
-              disabled={buttonDisabled}
-            >
-              {buttonText}
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+//   return (
+//     <section className="section">
+//       <div className="container">
+//         <div className="box">
+//           <div className="header">
+//             <p className="is-size-6 is-pulled-left">{'caption.account_not_active'}</p>
+//           </div>
+//           <div className="has-text-centered content">
+//             <p>{'prompt.account_not_active'}</p>
+//             <p>.</p>
+//             <button
+//               className="button is-link"
+//               onClick={sendActivateEmail}
+//               disabled={buttonDisabled}
+//             >
+//               {buttonText}
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Activate;
+// export default Activate;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,220 +1,220 @@
-'use client';
+// 'use client';
 
-import React, { useState, useEffect, ChangeEvent } from 'react';
-import Image from 'next/image';
-import '../styles/login.css';
-import axios from 'axios';
-import auth from '../utils/auth';
-import { useRouter } from 'next/navigation';
-import {getInvalidFields} from '../utils/validator';
-import '../globals.scss';
+// import React, { useState, useEffect, ChangeEvent } from 'react';
+// import Image from 'next/image';
+// import '../styles/login.css';
+// import axios from 'axios';
+// import auth from '../utils/auth';
+// import { useRouter } from 'next/navigation';
+// import {getInvalidFields} from '../utils/validator';
+// import '../globals.scss';
 
-interface InvalidFields {
-  email?: { message: string }[];
-  password?: { message: string }[];
-  captcha?: { message: string }[];
-}
+// interface InvalidFields {
+//   email?: { message: string }[];
+//   password?: { message: string }[];
+//   captcha?: { message: string }[];
+// }
 
-const Login = () => {
-  const router = useRouter();
-  const [captchaSrc, setCaptchaSrc] = useState('');
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    captcha: '',
-  });
-  const [invalidFields, setInvalidFields] = useState<InvalidFields>({});
-  const [isLogging, setisLogging] = useState(false);
+// const Login = () => {
+//   const router = useRouter();
+//   const [captchaSrc, setCaptchaSrc] = useState('');
+//   const [formData, setFormData] = useState({
+//     email: '',
+//     password: '',
+//     captcha: '',
+//   });
+//   const [invalidFields, setInvalidFields] = useState<InvalidFields>({});
+//   const [isLogging, setisLogging] = useState(false);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+//   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+//   };
 
-  const rules = () => ({
-    email: [
-      { required: true, message: ('validator.account_required') },
-      { type: 'email', message: ('validator.invalid_email') },
-    ],
-    password: [
-      { required: true, message: ('validator.password_required') },
-      { type: 'string', min: 6, max: 32, message: ('validator.invalid_password') },
-    ],
-    captcha: [
-      { required: true, message: ('validator.captcha_required') },
-    ],
-  });
+//   const rules = () => ({
+//     email: [
+//       { required: true, message: ('validator.account_required') },
+//       { type: 'email', message: ('validator.invalid_email') },
+//     ],
+//     password: [
+//       { required: true, message: ('validator.password_required') },
+//       { type: 'string', min: 6, max: 32, message: ('validator.invalid_password') },
+//     ],
+//     captcha: [
+//       { required: true, message: ('validator.captcha_required') },
+//     ],
+//   });
 
-  useEffect(() => {
-    updateCaptcha();
-  }, []);
+//   useEffect(() => {
+//     updateCaptcha();
+//   }, []);
 
-  const updateCaptcha = async () => {
-    const response = await axios.get('http://127.0.0.1:3000/api/v1/captcha');
-    setCaptchaSrc(response.data);
-  };
+//   const updateCaptcha = async () => {
+//     const response = await axios.get('http://127.0.0.1:3000/api/v1/captcha');
+//     setCaptchaSrc(response.data);
+//   };
 
-  const handleLogin = async () => {
-    const invalidFields = await getInvalidFields(formData, rules())
-    if (invalidFields) {
-      setInvalidFields(invalidFields);
-      return;
-    }
-    setisLogging(true);
-    try {
-      let response = await axios.post('http://127.0.0.1:3000/api/v1/auth/login', formData);
-      console.log('respone',response)
-      let userData = response.data || null;
-      if (auth.login(userData)) {
-        // 
-        //dispatch({ type: 'SET_USER', payload: auth.getUser() });
-        //dispatch({ type: 'SET_ISAUTHENTICATED', payload: auth.isAuthenticated() });
-        //let toPath = new URLSearchParams(window.location.search).get('redirect') || '/';
-        //navigate(toPath);
-      } else {
-        alert('Login failed, server returned incorrect data');
-      }
-      setisLogging(false);
-    } catch (error) {
-      alert(error || 'prompt.error_occurs');
-      setisLogging(false);
-    }
-  };
+//   const handleLogin = async () => {
+//     const invalidFields = await getInvalidFields(formData, rules())
+//     if (invalidFields) {
+//       setInvalidFields(invalidFields);
+//       return;
+//     }
+//     setisLogging(true);
+//     try {
+//       let response = await axios.post('http://127.0.0.1:3000/api/v1/auth/login', formData);
+//       console.log('respone',response)
+//       let userData = response.data || null;
+//       if (auth.login(userData)) {
+//         //
+//         //dispatch({ type: 'SET_USER', payload: auth.getUser() });
+//         //dispatch({ type: 'SET_ISAUTHENTICATED', payload: auth.isAuthenticated() });
+//         //let toPath = new URLSearchParams(window.location.search).get('redirect') || '/';
+//         //navigate(toPath);
+//       } else {
+//         alert('Login failed, server returned incorrect data');
+//       }
+//       setisLogging(false);
+//     } catch (error) {
+//       alert(error || 'prompt.error_occurs');
+//       setisLogging(false);
+//     }
+//   };
 
-  return (
-    <div>
-      <section className='section home'>
-        <div className='container login-wrap'>
-          <div className='columns'>
-            <div className='column has-text-white-bis is-hidden-mobile pt-200'>
-            </div>
-            <div className='column'>
-              <div className='card '>
-                <header className='card-header'>
-                  <p className='card-header-title is-centered'>
-                    Sign In
-                  </p>
-                </header>
-                <div className = 'card-content'>
-                  <div className = 'field'>
-                    <div className = 'control has-icons-left has-icons-right'>
-                      <input 
-                        className='input'
-                        type = 'email' 
-                        name='email'
-                        placeholder = 'placeholder.email' 
-                        value = {formData.email} 
-                        onChange = {handleChange}
-                      />
-                      <span className = 'icon is-small is-left'>
-                        <i className = 'fa fa-envelope'></i>
-                      </span>
-                    </div>
-                    {invalidFields.email && <p className='help is-danger'></p>}
-                  </div>
-                  <div className = 'field'>
-                    <div className = 'control has-icons-left'>
-                      <input 
-                        className='input'
-                        type='password' 
-                        name='password'
-                        placeholder = 'placeholder.password' 
-                        value = {formData.password} 
-                        onChange = {handleChange}
-                      />
-                      <span className = 'icon is-small is-left'>
-                        <i className = 'fa fa-lock'></i>
-                      </span>
-                    </div>
-                    {invalidFields.password && <p className='help is-danger'></p>}
-                  </div>
-                  <div className = 'field'>
-                    <div className = 'field is-grouped'>
-                      <p className = 'control is-expanded'>
-                        <input 
-                          className='input'
-                          type='text' 
-                          name='captcha'
-                          placeholder='placeholder.captcha' 
-                          value={formData.captcha} 
-                          onChange = {handleChange}
-                        />
-                      </p>
-                      <div className ='control' dangerouslySetInnerHTML={{ __html: captchaSrc }} 
-                        title={'prompt.click_refresh_captcha'} 
-                        onClick={updateCaptcha}
-                      />
-                    </div>
-                    {invalidFields.captcha && <p className='help is-danger'></p>}
-                  </div>
-                  <div className='field' style={{ marginTop: '30px' }}>
-                    <p className='control'>
-                      <button 
-                        className={`button is-link is-fullwidth is-focused ${isLogging ? 'is-loading' : ''}`}
-                        onClick={handleLogin}
-                        disabled={isLogging}>
-                        Sign In
-                      </button>
-                    </p>
-                  </div>
-                  <div className='field'>
-                    <div className='control forget-password'>
-                      <a href='/forgetPassword'>Forgot Password?</a>
-                    </div>
-                  </div>
-                </div>
-                <footer className='card-footer'>
-                  <p className='card-footer-item'>
-                    Don't have an account? 
-                    <a href='/signup'>Sign Up Now</a>
-                  </p>
-                </footer>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className='section feature'>
-        <nav className='columns'>
-          <div className='column has-text-centered'>
-            <div>
-              <p className='title is-4'>
-                <strong>Multiple Strategies</strong>
-              </p>
-              <p className='subtitle is-6'>Ordinary price investment, intelligent value investment, etc.</p>
-            </div>
-          </div>
-          <div className='column has-text-centered'>
-            <div>
-              <p className='title is-4'>
-                <strong>Strict Risk Control</strong>
-              </p>
-              <p className='subtitle is-6'>Secure storage and strict operation flow</p>
-            </div>
-          </div>
-          <div className='column has-text-centered'>
-            <div>
-              <p className='title is-4'>
-                <strong>Transparent Transactions</strong>
-              </p>
-              <p className='subtitle is-6'>One-click hosting, transparent API transactions</p>
-            </div>
-          </div>
-          <div className='column has-text-centered'>
-            <div>
-              <p className='title is-4'>
-                <strong>Open Data</strong>
-              </p>
-              <p className='subtitle is-6'>Multidimensional and intuitive visualization of investment data</p>
-            </div>
-          </div>
-        </nav>
-      </section>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <section className='section home'>
+//         <div className='container login-wrap'>
+//           <div className='columns'>
+//             <div className='column has-text-white-bis is-hidden-mobile pt-200'>
+//             </div>
+//             <div className='column'>
+//               <div className='card '>
+//                 <header className='card-header'>
+//                   <p className='card-header-title is-centered'>
+//                     Sign In
+//                   </p>
+//                 </header>
+//                 <div className = 'card-content'>
+//                   <div className = 'field'>
+//                     <div className = 'control has-icons-left has-icons-right'>
+//                       <input
+//                         className='input'
+//                         type = 'email'
+//                         name='email'
+//                         placeholder = 'placeholder.email'
+//                         value = {formData.email}
+//                         onChange = {handleChange}
+//                       />
+//                       <span className = 'icon is-small is-left'>
+//                         <i className = 'fa fa-envelope'></i>
+//                       </span>
+//                     </div>
+//                     {invalidFields.email && <p className='help is-danger'></p>}
+//                   </div>
+//                   <div className = 'field'>
+//                     <div className = 'control has-icons-left'>
+//                       <input
+//                         className='input'
+//                         type='password'
+//                         name='password'
+//                         placeholder = 'placeholder.password'
+//                         value = {formData.password}
+//                         onChange = {handleChange}
+//                       />
+//                       <span className = 'icon is-small is-left'>
+//                         <i className = 'fa fa-lock'></i>
+//                       </span>
+//                     </div>
+//                     {invalidFields.password && <p className='help is-danger'></p>}
+//                   </div>
+//                   <div className = 'field'>
+//                     <div className = 'field is-grouped'>
+//                       <p className = 'control is-expanded'>
+//                         <input
+//                           className='input'
+//                           type='text'
+//                           name='captcha'
+//                           placeholder='placeholder.captcha'
+//                           value={formData.captcha}
+//                           onChange = {handleChange}
+//                         />
+//                       </p>
+//                       <div className ='control' dangerouslySetInnerHTML={{ __html: captchaSrc }}
+//                         title={'prompt.click_refresh_captcha'}
+//                         onClick={updateCaptcha}
+//                       />
+//                     </div>
+//                     {invalidFields.captcha && <p className='help is-danger'></p>}
+//                   </div>
+//                   <div className='field' style={{ marginTop: '30px' }}>
+//                     <p className='control'>
+//                       <button
+//                         className={`button is-link is-fullwidth is-focused ${isLogging ? 'is-loading' : ''}`}
+//                         onClick={handleLogin}
+//                         disabled={isLogging}>
+//                         Sign In
+//                       </button>
+//                     </p>
+//                   </div>
+//                   <div className='field'>
+//                     <div className='control forget-password'>
+//                       <a href='/forgetPassword'>Forgot Password?</a>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <footer className='card-footer'>
+//                   <p className='card-footer-item'>
+//                     Don't have an account?
+//                     <a href='/signup'>Sign Up Now</a>
+//                   </p>
+//                 </footer>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//       <section className='section feature'>
+//         <nav className='columns'>
+//           <div className='column has-text-centered'>
+//             <div>
+//               <p className='title is-4'>
+//                 <strong>Multiple Strategies</strong>
+//               </p>
+//               <p className='subtitle is-6'>Ordinary price investment, intelligent value investment, etc.</p>
+//             </div>
+//           </div>
+//           <div className='column has-text-centered'>
+//             <div>
+//               <p className='title is-4'>
+//                 <strong>Strict Risk Control</strong>
+//               </p>
+//               <p className='subtitle is-6'>Secure storage and strict operation flow</p>
+//             </div>
+//           </div>
+//           <div className='column has-text-centered'>
+//             <div>
+//               <p className='title is-4'>
+//                 <strong>Transparent Transactions</strong>
+//               </p>
+//               <p className='subtitle is-6'>One-click hosting, transparent API transactions</p>
+//             </div>
+//           </div>
+//           <div className='column has-text-centered'>
+//             <div>
+//               <p className='title is-4'>
+//                 <strong>Open Data</strong>
+//               </p>
+//               <p className='subtitle is-6'>Multidimensional and intuitive visualization of investment data</p>
+//             </div>
+//           </div>
+//         </nav>
+//       </section>
+//     </div>
+//   );
+// };
 
-export default Login;
+// export default Login;
