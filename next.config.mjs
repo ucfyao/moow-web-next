5e214dfc8f2ca1d66ced12411bef50ad1325ef74
wjtwzgl12 <jintaowang2022@163.com>
2024-07-04 08:36:25 +1000

    fix: #409 cors problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+import { i18n } from './next-i18next.config.js';
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:3000/api/:path*', 
+        },
+      ];
+    },
+    i18n,
+  };
+  
+  export default nextConfig;
