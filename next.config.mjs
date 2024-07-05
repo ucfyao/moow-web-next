2baf0c9c383746bda7e2c7da7c4d2921d02c2bbd
wjtwzgl12 <jintaowang2022@163.com>
2024-07-05 13:24:33 +1000

    fix: #409 development cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 import { i18n } from './next-i18next.config.js';
 const nextConfig = {
-    i18n,
-  };
+  async rewrites() {
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:3000/api/:path*',
+        },
+      ];
+    } else {
+      return [];
+    }
+  },
+  i18n,
+};
   
-  export default nextConfig;
+export default nextConfig;
