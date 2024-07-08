caeec237104f609ffd71e2a214a9f4edd0e6deba
wjtwzgl12 <jintaowang2022@163.com>
2024-07-09 08:56:30 +1000

    feat: #413 tip component

--- src/component/tip.tsx
+++ src/component/tip.tsx
@@ -1,13 +1,14 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
-import './Tip.css';
+import { css } from '@emotion/react';
 
 const Tip: React.FC<{ content: React.ReactNode }> = ({ content }) => {
   return (
-    <div className="poptip-popper">
+    <div css={poptipPopper}>
       <div className="poptip-content">
-        <div className="poptip-arrow"></div>
-        <div className="poptip-inner">
-          <div className="poptip-body">
+        <div css={poptipArrow}></div>
+        <div css={poptipInner}>
+          <div css={poptipBody}>
             <div className="poptip-body-content">
               {content}
             </div>
@@ -17,5 +18,64 @@ const Tip: React.FC<{ content: React.ReactNode }> = ({ content }) => {
     </div>
   );
 };
+
+const poptipPopper = css`
+  position: absolute;
+  visibility: hidden;
+  min-width: 150px;
+  font-size: 12px;
+  line-height: 1.5;
+  z-index: 1060;
+  padding: 5px 0 8px 0;
+  position: absolute;
+  will-change: top, left;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -100%);
+  opacity: 0;
+  z-index: 0;
+`;
+
+const poptipArrow = css`
+  display: block;
+  width: 0;
+  height: 0;
+  position: absolute;
+  left: 50%;
+  margin-left: -5px;
+  bottom: 3px;
+  border-width: 5px 5px 0;
+  border-top-color: rgba(217, 217, 217, 0.5);
+  border-color: transparent;
+  border-style: solid;
+
+  &:after {
+    position: absolute;
+    display: block;
+    width: 0;
+    height: 0;
+    content: " ";
+    top: -6px;
+    bottom: 1px;
+    margin-left: -5px;
+    border-width: 5px;
+    border-color: transparent;
+    border-top-color: #fff;
+    border-style: solid;
+  }
+`;
+
+const poptipInner = css`
+  width: 100%;
+  background-color: #fff;
+  background-clip: padding-box;
+  border-radius: 4px;
+  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2);
+  white-space: nowrap;
+`;
+
+const poptipBody = css`
+  padding: 8px 12px;
+`;
 
 export default Tip;
